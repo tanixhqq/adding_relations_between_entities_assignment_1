f2c923258dc87803f5b123533e819e09a75cebf4
tanishq singhal <tanishqsinghal483@gmail.com>
2025-03-19 14:47:08 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import DestinationContainer from './components/DestinationContainer';
+
 function App() {
   return (
-    <div>
-      {/* Your code here */}
+    <div className="App">
+      <h1 className="text-3xl font-bold text-center my-6">🌍 Top Travel Destinations</h1>
+      <DestinationContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
